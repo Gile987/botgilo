3925eb0cca34424a1f9d966cbd53a8b9ed582ddb
Gile <mladen.reljic87@gmail.com>
2024-04-07 17:46:43 +0200

    Edit the help message

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,5 +1,11 @@
-function helpHandler(bot, chatId, args) {
-  bot.sendMessage(chatId, 'Ne mogu pomoci ni samom sebi :(');
+function helpHandler(bot, chatId) {
+  const message = `
+    Here are the available commands:
+
+    1. /help: Display this help message.
+    2. /weather <city>: Get the current weather for the specified city.
+  `;
+  bot.sendMessage(chatId, message);
 }
 
 module.exports = helpHandler;
